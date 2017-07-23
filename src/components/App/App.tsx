11f30234710422723c4ee6177f591a7816cdc3c5
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-07-23 16:42:58 -0700

    Add IncomeForm component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import SplashScreen from '../SplashScreen'
 import SiteHeader from '../SiteHeader'
+import IncomeForm from '../IncomeForm'
 import './App.css'
 
 class App extends React.Component<{}, {}> {
@@ -8,7 +9,8 @@ class App extends React.Component<{}, {}> {
     return (
       <div className="App">
         <SiteHeader />
-        <SplashScreen shouldComponentBeHidden={false}/>
+        <IncomeForm />
+        <SplashScreen shouldComponentBeHidden={true}/>
       </div>
     )
   }
---
+++ src/components/IncomeForm/IncomeForm.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default class IncomeForm extends React.Component<{}, {}> {
+  render (): JSX.Element {
+    return (
+      <div className="IncomeForm">
+        Income Form
+      </div>
+    )
+  }
+}
