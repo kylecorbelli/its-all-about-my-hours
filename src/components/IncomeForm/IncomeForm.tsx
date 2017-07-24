bf5f71308eb5d11efbdc492a9e452210870b1cc0
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-07-24 13:40:32 -0700

    Style income form

--- src/components/IncomeForm/IncomeForm.tsx
+++ src/components/IncomeForm/IncomeForm.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import SplashScreen from '../SplashScreen'
-import { Link } from 'react-router-dom'
+import './IncomeForm.css'
 
 interface Props {
   readonly history: {
@@ -40,24 +40,32 @@ export default class IncomeForm extends React.Component<Props, State> {
     return (
       <div className="IncomeForm">
         <form className="IncomeForm__form" onSubmit={this.onSubmitForm}>
-          <input
-            className="IncomeForm__text-input"
-            type="number"
-            onChange={this.handleRegularPayInputChange}
-            ref={element => this.regularPayInput = element}
-            placeholder="Gross regular-time pay"
-          />
-          <br />
-          <input
-            className="IncomeForm__text-input"
-            type="number"
-            onChange={this.handleOvertimePayInputChange}
-            placeholder="Gross overtime pay"
-          />
-          <br />
-          <button type="submit">Calculate</button>
+          <p className="IncomeForm__form-headline">Calculate How Many Hours You’ve Accumulated:</p>
+          <label>
+            <div className="IncomeForm__input-group">
+              <div className="IncomeForm__dollar-symbol">&#36;</div>
+              <input
+                className="IncomeForm__text-input"
+                type="number"
+                onChange={this.handleRegularPayInputChange}
+                ref={element => this.regularPayInput = element}
+                placeholder="Gross regular-time pay"
+              />
+            </div>
+          </label>
+          <label>
+            <div className="IncomeForm__input-group">
+              <div className="IncomeForm__dollar-symbol">&#36;</div>
+              <input
+                className="IncomeForm__text-input"
+                type="number"
+                onChange={this.handleOvertimePayInputChange}
+                placeholder="Gross overtime pay"
+              />
+            </div>
+          </label>
+          <button className="IncomeForm__submit-button" type="submit">Calculate</button>
         </form>
-        <Link to="/hours-results" >Results Page</Link>
         <SplashScreen shouldComponentBeHidden={false} />
       </div>
     )
