aec65db9e5453d4cc99904ae333c86f27a876b5f
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-07-23 17:03:20 -0700

    Add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import App from './components/App'
+// import App from './components/App'
+import Routes from './routes'
 import './styles/index.css'
 
 ReactDOM.render(
-  <App />,
+  <Routes />,
   document.getElementById('root') as HTMLElement
 )
