f9b4bcdfe4433eec8a270b170cdb9360fe132050
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-07-29 10:51:04 -0700

    Rething types

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -8,12 +8,15 @@ export type ReduxActionTypesEnum = 'SET_SPLASH_SCREEN_HAS_BEEN_SHOWN'
   | 'CALCULATE_HOURS_WORKED----CHANGE_THIS_OBVIOUSLY'
   | 'CALCULATE_EXPECTED_FINISH_DATE----CHANGE_THIS_OBVIOUSLY'
 
-export const ReduxActionTypes = {
-  SET_SPLASH_SCREEN_HAS_BEEN_SHOWN: 'SET_SPLASH_SCREEN_HAS_BEEN_SHOWN' as ReduxActionTypesEnum
+export namespace ReduxActionTypes  {
+  export const SET_SPLASH_SCREEN_HAS_BEEN_SHOWN: ReduxActionTypesEnum = 'SET_SPLASH_SCREEN_HAS_BEEN_SHOWN'
 }
 
-export interface SetSplashScreenHasBeenShownAction {
+export interface BaseReduxAction {
   readonly type: ReduxActionTypesEnum
+}
+
+export interface SetSplashScreenHasBeenShownAction extends BaseReduxAction  {
   readonly payload: {
     readonly hasSplashScreenBeenShown: boolean
   }
