3c0c1d7fdd29032e374eaff5960771cc40c57899
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-07-30 05:40:30 -0700

    Add hours-calculations service

---
+++ src/services/hours-calculations/index.ts
@@ -0,0 +1,22 @@
+const TARGET_HOURS = 2000
+const HOURLY_REGULAR_TIME_RATE = 15
+const OVERTIME_RATE = HOURLY_REGULAR_TIME_RATE * 1.5
+
+export const calculateRegularTimeHoursWorked = (
+  grossRegularTimePay: number,
+): number => grossRegularTimePay / HOURLY_REGULAR_TIME_RATE
+
+export const calculateOvertimeHoursWorked = (
+  grossOvertimePay: number,
+): number => grossOvertimePay / OVERTIME_RATE
+
+export const calculateTotalHoursWorked = (
+  grossRegularTimePay: number,
+  grossOvertimePay: number,
+): number => calculateRegularTimeHoursWorked(grossRegularTimePay) +
+             calculateOvertimeHoursWorked(grossOvertimePay)
+
+export const calculateTotalHoursRemaining = (
+  grossRegularTimePay: number,
+  grossOvertimePay: number,
+): number => TARGET_HOURS - calculateTotalHoursWorked(grossRegularTimePay, grossOvertimePay)
