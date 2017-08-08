361575d84b91dc93fe9d5629f34b93df6c3506df
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-08-08 08:05:33 -0700

    Add code coverage

--- src/containers/HoursResultsConnected.tsx
+++ src/containers/HoursResultsConnected.tsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import HoursResults from '../components/HoursResults'
 import { ReduxState } from '../redux/types'
 
-const mapStateToProps = (state: ReduxState) => {
+export const mapStateToProps = (state: ReduxState) => {
   const {
     hasCompletedIncomeForm,
     grossRegularTimePay,
