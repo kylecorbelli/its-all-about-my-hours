39c8ac9e2a51ec2354977014f5170a578a58e4bf
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-08-10 09:38:47 -0700

    Add utility function to calculate expected completion date

--- src/services/hours-calculations/index.test.ts
+++ src/services/hours-calculations/index.test.ts
@@ -3,34 +3,96 @@ import {
   calculateOvertimeHoursWorked,
   calculateTotalHoursWorked,
   calculateTotalHoursRemaining,
+  calculateNumberOfWeeksBetween,
+  calculateAverageHoursWorkedPerWeek,
+  calculateNumberOfWeeksStillNeeded,
+  calculateNWeeksLaterDate,
+  calculateExpectedCompletionDate,
 } from './index'
 
 describe('hoursCalculations', () => {
   describe('calculateRegularTimeHoursWorked', () => {
     it('calculates the number of hours worked given gross regular time pay', () => {
-      expect(calculateRegularTimeHoursWorked(6789)).toBe(452.6)
+      expect(calculateRegularTimeHoursWorked(6789)).toBeCloseTo(452.6)
     })
   })
 
   describe('calculateOvertimeHoursWorked', () => {
     it('calculates the number of hours worked given gross overtime pay', () => {
-      expect(calculateOvertimeHoursWorked(345.67)).toBe(15.363111111111111)
+      expect(calculateOvertimeHoursWorked(345.67)).toBeCloseTo(15.363111111111111)
     })
   })
 
   describe('calculateTotalHoursWorked', () => {
     it('calculates the total number of hours worked given gross regular time pay and gross overtime pay', () => {
-      expect(calculateTotalHoursWorked(9876, 765.43)).toBe(692.4191111111111)
+      expect(calculateTotalHoursWorked(9876, 765.43)).toBeCloseTo(692.4191111111111)
     })
   })
 
   describe('calculateTotalHoursRemaining', () => {
     it('calculates the total number of hours remaining given the total number of hours worked so far', () => {
-      expect(calculateTotalHoursRemaining(9876, 765.43)).toBe(1307.580888888889)
+      expect(calculateTotalHoursRemaining(9876, 765.43)).toBeCloseTo(1307.580888888889)
     })
 
     it('returns 0 when the number of hours worked so far exceeds the target hours', () => {
       expect(calculateTotalHoursRemaining(34567, 789)).toBe(0)
     })
+  })
+
+  describe('calculateNumberOfWeeksBetween', () => {
+    it('calculates the number of weeks between two dates', () => {
+      const startDate: Date = new Date(2017, 7, 3)
+      const endDate: Date = new Date(2017, 7, 10)
+      const result: number = calculateNumberOfWeeksBetween(startDate, endDate)
+      expect(result).toBe(1)
+    })
+  })
+
+  describe('calculateAverageHoursWorkedPerWeek', () => {
+    it('calculates the average hours worked per week', () => {
+      const grossRegularTimePay: number = 1065
+      const grossOvertimePay: number = 45
+      const currentDate: Date = new Date(2017, 3, 24)
+      const result: number = calculateAverageHoursWorkedPerWeek(grossRegularTimePay, grossOvertimePay, currentDate)
+      expect(result).toBe(36.50)
+    })
+  })
+
+  describe('calculateNumberOfWeeksStillNeeded', () => {
+    const grossRegularTimePay: number = 1065
+    const grossOvertimePay: number = 45
+    const currentDate: Date = new Date(2017, 3, 24)
+    const result: number = calculateNumberOfWeeksStillNeeded(grossRegularTimePay, grossOvertimePay, currentDate)
+    expect(result).toBeCloseTo(52.7945205479452) // or whatever the average hours per weeks is
+  })
+
+  
+  describe('calculateNWeeksLaterDate', () => {
+    it('calculates the date some integer number of weeks from a given start date', () => {
+      const startDate: Date = new Date(2017, 3, 10)
+      const result: Date = calculateNWeeksLaterDate(startDate, 2)
+      const resultDateString: string = result.toDateString()
+      const expectedDateString: string = new Date(2017, 3, 24).toDateString()
+      expect(resultDateString).toBe(expectedDateString)
+    })
+
+    it('calculates the date some fractional number of weeks from a given start date', () => {
+      const startDate: Date = new Date(2017, 3, 10)
+      const result: Date = calculateNWeeksLaterDate(startDate, 2.2857142857142857)
+      const resultDateString: string = result.toDateString()
+      const expectedDateString: string = new Date(2017, 3, 26).toDateString()
+      expect(resultDateString).toBe(expectedDateString)
+    })
+  })
+
+  describe('calculateExpectedCompletionDate', () => {
+    const currentDate: Date = new Date(2017, 3, 24)
+    const grossRegularTimePay: number = 1065
+    const grossOvertimePay: number = 45
+    const result: Date = calculateExpectedCompletionDate(grossRegularTimePay, grossOvertimePay, currentDate)
+    const resultDateString: string = result.toDateString()
+    const expectedCompletionDate: Date = new Date(2018, 3, 28)
+    const expectedCompletionDateDateString: string = expectedCompletionDate.toDateString()
+    expect(resultDateString).toBe(expectedCompletionDateDateString)
   })
 })
--- src/services/hours-calculations/index.ts
+++ src/services/hours-calculations/index.ts
@@ -1,6 +1,12 @@
 const TARGET_HOURS: number = 2000
 const HOURLY_REGULAR_TIME_RATE: number = 15
 const OVERTIME_RATE: number = HOURLY_REGULAR_TIME_RATE * 1.5
+const MILLISECONDS_PER_SECOND: number = 1000
+const SECONDS_PER_MINUTE: number = 60
+const MINUTES_PER_HOUR: number = 60
+const HOURS_PER_DAY: number = 24
+const DAYS_PER_WEEK: number = 7
+const START_DATE: Date = new Date(2017, 3, 10) // Get the actual date
 
 export const calculateRegularTimeHoursWorked = (
   grossRegularTimePay: number,
@@ -22,4 +28,52 @@ export const calculateTotalHoursRemaining = (
 ): number => {
   const totalHoursWorked: number = calculateTotalHoursWorked(grossRegularTimePay, grossOvertimePay)
   return Math.max(TARGET_HOURS - totalHoursWorked, 0)
+}
+
+export const calculateNumberOfWeeksBetween = (startDate: Date, endDate: Date): number => {
+  const startDateWithoutTime: Date = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate())
+  const endDateWithoutTime: Date = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate())
+  const millisecondsBetweenDates: number = endDateWithoutTime.valueOf() - startDateWithoutTime.valueOf()
+  const secondsBetweenDates: number = millisecondsBetweenDates / MILLISECONDS_PER_SECOND
+  const minutesBetweenDates: number = secondsBetweenDates / SECONDS_PER_MINUTE
+  const hoursBetweenDates: number = minutesBetweenDates / MINUTES_PER_HOUR
+  const daysDetweenDates: number = hoursBetweenDates / HOURS_PER_DAY
+  return daysDetweenDates / DAYS_PER_WEEK
+}
+
+export const calculateAverageHoursWorkedPerWeek = (
+  grossRegularTimePay: number,
+  grossOvertimePay: number,
+  currentDate: Date,
+): number => {
+  const totalHoursWorked: number = calculateTotalHoursWorked(grossRegularTimePay, grossOvertimePay)
+  const numberOfWeeksElapsed: number = calculateNumberOfWeeksBetween(START_DATE, currentDate)
+  return totalHoursWorked / numberOfWeeksElapsed
+}
+
+export const calculateNumberOfWeeksStillNeeded = (
+  grossRegularTimePay: number,
+  grossOvertimePay: number,
+  currentDate: Date,
+): number => {
+  const totalHoursRemaining: number = calculateTotalHoursRemaining(grossRegularTimePay, grossOvertimePay)
+  const averageHoursWorkedPerWeek: number = calculateAverageHoursWorkedPerWeek(grossRegularTimePay, grossOvertimePay, currentDate)
+  return totalHoursRemaining / averageHoursWorkedPerWeek
+}
+
+export const calculateNWeeksLaterDate = (startDate: Date, numberOfWeeksFromNow: number): Date => {
+  const newDate: Date = new Date(startDate.getTime())
+  const numberOfDaysLater: number = numberOfWeeksFromNow * DAYS_PER_WEEK
+  newDate.setDate(startDate.getDate() + numberOfDaysLater)
+  return newDate
+}
+
+export const calculateExpectedCompletionDate = (
+  grossRegularTimePay: number,
+  grossOvertimePay: number,
+  currentDate: Date,
+): Date => {
+  const numberOfWeeksStillNeeded: number = calculateNumberOfWeeksStillNeeded(grossRegularTimePay, grossOvertimePay, currentDate)
+  console.log(numberOfWeeksStillNeeded)
+  return calculateNWeeksLaterDate(currentDate, numberOfWeeksStillNeeded)
 }
